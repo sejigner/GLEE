ef20c3337bc1a7ffe94bd1ecd3a39853e3a41a95
sejigner <sejigner@gmail.com>
2021-09-11 14:59:30 +0900

    Add ActivityLicense and ActivityOpenSource
    Fix #22

---
+++ app/src/main/java/com/sejigner/glee/OpenSourceActivity.kt
@@ -0,0 +1,11 @@
+package com.sejigner.glee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenSourceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_source)
+    }
+}
